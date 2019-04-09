47376d7a253e7d8807f269cb6cdb18faa32943b1
mady51 <root@instance-1.us-east1-b.c.polar-cyclist-235904.internal>
2019-04-09 06:39:30 +0000

    cpufreq: interactive: Don't set floor_validate_time during boost

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -778,8 +778,6 @@ static void cpufreq_interactive_boost(struct cpufreq_interactive_tunables *tunab
 		 * validated.
 		 */
 
-		ppol->floor_freq = tunables->hispeed_freq;
-		ppol->floor_validate_time = ktime_to_us(ktime_get());
 		spin_unlock_irqrestore(&ppol->target_freq_lock, flags[1]);
 		break;
 	}
